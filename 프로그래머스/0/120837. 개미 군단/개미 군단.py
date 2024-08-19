70b6280d7cab78898591f30bd345be6bc173a4c1
august <55475872+august-hw2@users.noreply.github.com>
2024-08-19 21:59:07 +0900

    [level 0] Title: 개미 군단, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.py
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.py
@@ -1,14 +1,4 @@
 def solution(hp):
-    res = int(hp/5)
     
-    hp -= 5*int(hp/5)
-    
-    if hp >= 3:
-        res += int(hp/3)
-        hp -= 3*int(hp/3)
-    
-    if hp >= 1:
-        res += int(hp/1)
-        
-    return res
+    return hp//5 + hp%5//3 + hp%5%3
         
