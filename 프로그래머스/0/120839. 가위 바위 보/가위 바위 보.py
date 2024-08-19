c0b9d100d668657babea7fae56aeefc06052ec7a
august <55475872+august-hw2@users.noreply.github.com>
2024-08-19 22:26:42 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,9 +1,5 @@
 def solution(rsp):
     
     win = {"2":"0", "0":"5", "5":"2"}
-    res = ""
     
-    for i in rsp:
-        res += win.get(i)
-        
-    return res
+    return ''.join(win[i] for i in rsp)
