084b0fcaaf547ee399574b5720c44ad615469993
august <55475872+august-hw2@users.noreply.github.com>
2024-08-21 21:37:49 +0900

    [level 0] Title: k의 개수, Time: 40.15 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.py
+++ 프로그래머스/0/120887. k의 개수/k의 개수.py
@@ -1,10 +1,3 @@
 def solution(i, j, k):
-    res = 0
-
-    for i in range(i, j+1):
-        tmp = list(map(int, str(i)))
-        if k in tmp:
-            res += tmp.count(k)
-            
-    return res
+    return sum([str(i).count(str(k)) for i in range(i, j+1)])
             
