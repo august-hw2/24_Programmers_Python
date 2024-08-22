1b47af1cb447ae0118600664daf41324dbb120e9
august <55475872+august-hw2@users.noreply.github.com>
2024-08-22 16:57:45 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -0,0 +1,6 @@
+def solution(my_string):
+    for i in my_string:
+        if i.isalpha():
+            my_string = my_string.replace(i, ' ')
+            
+    return sum([int(i) for i in my_string.split(' ') if i])
