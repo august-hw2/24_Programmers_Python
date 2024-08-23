f8cc73c933b0f4cf1a4533195414500bdcf66e6c
august <55475872+august-hw2@users.noreply.github.com>
2024-08-23 23:58:19 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.09 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,8 +1,6 @@
 def solution(numbers):
-    num_dict = {'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4',
-       'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
-
-    for k, v in num_dict.items():
-        numbers = numbers.replace(k, v)
+    for k, v in enumerate(['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']):
+        numbers = numbers.replace(v, str(k))
+        #enumerate(값, 인덱스 번호)
 
     return int(numbers)
