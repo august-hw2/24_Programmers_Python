8a0f216e4244ea80f69d23df7824cf6f3b01c601
august <55475872+august-hw2@users.noreply.github.com>
2024-08-23 22:30:35 +0900

    [level 0] Title: 소인수분해, Time: 2.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.py
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -11,7 +11,7 @@ def solution(n):
             if i%j == 0:
                 cnt += 1
 
-        if cnt >= 3:
+        if cnt >= 3: #소인수는 약수로 자기자신과 1만 있기 때문에, 3 이상의 약수를 가진다면 소인수에 해당하지 않음
             res[res.index(i)] = 0
 
     return [i for i in res if i!=0]
