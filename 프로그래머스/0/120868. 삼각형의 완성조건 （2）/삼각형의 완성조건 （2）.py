970a38965c75f868e956b75b86f7117dd479332e
august <55475872+august-hw2@users.noreply.github.com>
2024-08-24 16:08:25 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
@@ -1,3 +1,21 @@
 def solution(sides):
     return 2*min(sides)-1
+    '''
+    sides에서 a > b일 때
     
+    1) a+b > c 
+       a+b > c > a 여야 한다.
+       정리하면, 
+       b-1 > c > 0 이므로 (b-1)개
+    
+    2) b+c > a
+       b+c > a > c 여야 한다.
+       정리하면, 
+       b-1 > a > 0 이므로 (b-1)개
+    
+    3) a = c
+       3번의 경우는 1개
+       
+       => (b-1) + (b-1) + 1 = (2b-1)개
+    
+    '''
