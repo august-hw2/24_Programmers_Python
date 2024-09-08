3e6eeee3fc8a2afa2a362685cd4469ee30df5b9a
august <55475872+august-hw2@users.noreply.github.com>
2024-09-08 18:10:21 +0900

    [level 0] Title: OX퀴즈, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
@@ -5,11 +5,9 @@ def solution(quiz):
     for i in quiz:
         tmp = i.split(" ")
         if '+' in tmp: #덧셈
-            add = int(tmp[0]) + int(tmp[2])
-            ans.append("O") if add == int(tmp[4]) else ans.append("X")
+            ans.append("O") if int(tmp[0]) + int(tmp[2]) == int(tmp[4]) else ans.append("X")
 
         elif '-' in tmp: #뺄셈
-            sub = int(tmp[0]) - int(tmp[2])
-            ans.append("O") if sub == int(tmp[4]) else ans.append("X")
+            ans.append("O") if int(tmp[0]) - int(tmp[2]) == int(tmp[4]) else ans.append("X")
 
     return ans
