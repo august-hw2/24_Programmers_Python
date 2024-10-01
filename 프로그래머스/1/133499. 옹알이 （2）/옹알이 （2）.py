9bd7098d03757c557d6dc15025a27b2538d68283
august <55475872+august-hw2@users.noreply.github.com>
2024-10-01 21:19:06 +0900

    [level 1] Title: 옹알이 (2), Time: 0.36 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,25 +1,3 @@
 def solution(babbling):
-    ans = 0
-    init = {"aya", "ye", "woo", "ma"}
-
-    for word in babbling:
-        com = ""  #비교할 단어
-        tmp = ""  #임시 저장할 단어
-
-        for char in word:
-            com += char
-
-            #연속해서 같은 단어가 나왔을 때 반복문 탈출
-            if com == tmp:
-                break
-
-            #4가지 단어가 포함 돼있을 때 현재 단어 임시 저장 후 비교단어 초기화
-            if com in init:
-                tmp = com
-                com = ""
-
-        #비교할 단어가 존재 하지 않을때 카운트
-        if com == "":
-            ans += 1
-
-    return ans
+    answer = 0
+    return answer
