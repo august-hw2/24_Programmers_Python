71b35ca3eac4c425cf41c851d7fcb688506a56d1
august <55475872+august-hw2@users.noreply.github.com>
2024-11-09 21:52:06 +0900

    [level 2] Title: 올바른 괄호, Time: 6.98 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,16 +1,10 @@
 def solution(s):
-
     stack = []
-
     for char in s:
-        #스택에 아무것도 없는데, 닫힌 괄호 들어올 때
-        if len(stack) == 0 and char == ')':
-            return False
-
-        if char == ')' and stack[-1] == '(':
-            stack.pop()
-
         if char == '(':
             stack.append(char)
-
-    return False if len(stack) else True
+        else:
+            if not stack:
+                return False
+            stack.pop()
+    return not stack
